af54ec0bac927b46ececeb9d9b700ca5753d122a
Deena <deena.dayalan@atmel.com>
2016-03-07 02:13:29 +0530

    Compute margin on the shares

--- share_csv_parser.py
+++ share_csv_parser.py
@@ -3,6 +3,45 @@ from datetime import datetime
 
 import ScripDetail
 
+def computeMargin(scripName, scripDetails = []):
+    sellOrders = [sellOrder for sellOrder in scripDetails if sellOrder.orderType == 'SELL']
+    buyOrders = [buyOrder for buyOrder in scripDetails if buyOrder.orderType == 'BUY']
+    totalSoldQuantity = sum(c.quantity for c in sellOrders)
+    soldAmount = 0.0
+    boughtAmount = 0.0
+    totalBoughtAmount = 0.0
+    totalMargin = 0.0
+    totalTransactionAmount = 0.0
+
+    if totalSoldQuantity > 0:
+        for sellOrder in sellOrders:
+            for buyOrder in buyOrders:
+                if(buyOrder.remainingQuantity > 0 and sellOrder.remainingQuantity > 0):
+                    soldQuantityRemaining = sellOrder.remainingQuantity
+                    if buyOrder.remainingQuantity > soldQuantityRemaining:
+                        soldAmount = (soldQuantityRemaining * sellOrder.price)
+                        boughtAmount = (buyOrder.price * soldQuantityRemaining)
+                        buyOrder.remainingQuantity = buyOrder.remainingQuantity - soldQuantityRemaining
+                        sellOrder.remainingQuantity = 0
+                    elif buyOrder.remainingQuantity < soldQuantityRemaining:
+                        soldAmount = (buyOrder.remainingQuantity * sellOrder.price)
+                        boughtAmount = (buyOrder.price * buyOrder.remainingQuantity)
+                        sellOrder.remainingQuantity = sellOrder.remainingQuantity - buyOrder.remainingQuantity
+                        buyOrder.remainingQuantity = 0
+                    else:
+                        soldAmount = (sellOrder.remainingQuantity * sellOrder.price)
+                        boughtAmount = (buyOrder.price * buyOrder.remainingQuantity)
+                        buyOrder.remainingQuantity = sellOrder.remainingQuantity = 0
+
+                    totalBoughtAmount += boughtAmount
+                    totalTransactionAmount +=  (soldAmount + boughtAmount)
+                    totalMargin += (soldAmount - boughtAmount)
+
+        #print('Total boughtAmount : ', totalBoughtAmount)
+        #print('Total transaction Amount : ', totalTransactionAmount)
+        if totalBoughtAmount > 0 :
+            print(key , " : ", (totalMargin * 100)/totalBoughtAmount)
+            
 dict = {}
 with open ('Shares.csv', newline='') as csvfile:
     totalValidLines = 0
@@ -16,10 +55,10 @@ with open ('Shares.csv', newline='') as csvfile:
                 #This is the actual line which contains information
                 if row[2] in dict:
                     #This scrip is already available in the dictionary
-                    dict[row[2]].append(ScripDetail.make_ScripDetail(row[1], row[3], row[4], row[5], row[6]))
+                    dict[row[2]].append(ScripDetail.make_ScripDetail(row[1], row[3], int(row[4]), float(row[5]), float(row[6])))
                 else:
                     dict[row[2]] = [];
-                    dict[row[2]].append(ScripDetail.make_ScripDetail(row[1], row[3], row[4], row[5], row[6]))
+                    dict[row[2]].append(ScripDetail.make_ScripDetail(row[1], row[3], int(row[4]), float(row[5]), float(row[6])))
         else:
             print('No. of colums in the csv file is not equal to 8')
             exit()
@@ -55,16 +94,18 @@ for key in dict.keys():
     scripsBoughtNo = 0;
     scripAverageBoughtPrice = 0;
 
+    computeMargin(key, scripDetails)
+
     for detail in scripDetails:
         #adding up sell together
         if detail.orderType == 'SELL':
-            scripTotalSell += float(detail.totalPrice)
-            scripsSoldNo += -int(detail.quantity)
+            scripTotalSell += detail.totalPrice
+            scripsSoldNo += detail.quantity
         #adding up buy together
         if detail.orderType == 'BUY':
             #Since BUY items are marked as negative making them positive by negating it with minus symbol
-            scripTotalBuy += -float(detail.totalPrice)
-            scripsBoughtNo += int(detail.quantity)
+            scripTotalBuy += detail.totalPrice
+            scripsBoughtNo += detail.quantity
 
     if scripsBoughtNo > 0 and scripsSoldNo > 0 :
         scripAverageBoughtPrice = scripTotalBuy/scripsBoughtNo
@@ -78,4 +119,3 @@ for key in dict.keys():
 print('Total capital outflow : ', "%.2f" %totalBuy)
 print('Total capital inflow  : ', "%.2f" %totalSell)
 print('Total investment     : ', "%.2f" %(totalBuy - totalSell))
-
